16f84aac34bd73d43b09f656eda58166169ff66e
NhonAleph <nhon.nguyen@aleph-labs.com>
2020-03-16 00:35:28 +0700

    change api from http to https, make api working on public github site

--- src/constants/temp.js
+++ src/constants/temp.js
@@ -4,7 +4,7 @@ export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const CHANGE_KEYWORD = 'CHANGE_KEYWORD'
 export const APIKey = '845245a630a54a6fb27cda4c1e90c7f2'
-export const baseURL = 'http://newsapi.org/v2/'
+export const baseURL = 'https://newsapi.org/v2/'
 export const listKeywords = [
   {
     label: 'bitcoin',
