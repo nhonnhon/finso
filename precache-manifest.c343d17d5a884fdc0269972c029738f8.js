536f64715e245b26b14a7a0b3bb1b9ab1147c832
NhonAleph <nhon.nguyen@aleph-labs.com>
2020-03-16 00:14:45 +0700

    Updates

---
+++ precache-manifest.c343d17d5a884fdc0269972c029738f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c92224fc655b4ce2e450d3e966b2607",
+    "url": "/finso/index.html"
+  },
+  {
+    "revision": "767a6c497ea64edefbee",
+    "url": "/finso/static/css/main.873549ea.chunk.css"
+  },
+  {
+    "revision": "ce5e688eee3de7d4944a",
+    "url": "/finso/static/js/2.bc634e1c.chunk.js"
+  },
+  {
+    "revision": "767a6c497ea64edefbee",
+    "url": "/finso/static/js/main.aeb5950a.chunk.js"
+  },
+  {
+    "revision": "c1c7f5a25366ec6af5bb",
+    "url": "/finso/static/js/runtime-main.af547c8e.js"
+  },
+  {
+    "revision": "ef2915c09893b486e5568f2990913bea",
+    "url": "/finso/static/media/collapsible-arrow.ef2915c0.svg"
+  }
+]);
